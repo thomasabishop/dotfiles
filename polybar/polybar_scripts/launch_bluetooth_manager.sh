942bd9bd66d21132b2685b8098b95341c2b579f6
tactonbishop <tactonbishop@gmail.com>
2022-06-11 13:32:44 +0100

    Ch ch ch changes

--- polybar/polybar_scripts/launch_bluetooth_manager.sh
+++ polybar/polybar_scripts/launch_bluetooth_manager.sh
@@ -2,11 +2,23 @@
 #!/bin/sh
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
 then
-  echo "%{F#66ffffff}"
+    echo "%{F#66ffffff}"
 else
-  if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
-  then 
-    echo ""
-  fi
-  echo "%{F#73fa91}"
+    if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
+    then
+        echo ""
+    fi
+    echo "%{F#73fa91}"
 fi
+
+
+
+# if [ "$player_status" = "Playing" ]; then
+#     echo " $(playerctl metadata artist) | $(playerctl metadata title) | $(playerctl metadata album)"
+# elif [ "$player_status" = "Paused" ]; then
+#     echo " $(playerctl metadata artist) | $(playerctl metadata title) | $(playerctl metadata album)"
+# elif [ "$player_status" = "Stopped" ]; then
+#     echo " $(playerctl metadata artist) | $(playerctl metadata title) | $(playerctl metadata album)"
+# else
+#     echo " Disconnected"
+# fi
