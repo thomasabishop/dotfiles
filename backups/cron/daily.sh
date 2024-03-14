130c3a5aee9feacd72ff38004cbeea83f2113e05
thomasabishop <thomasabishop@proton.me>
2024-03-14 17:19:26 +0000

    backups: improve cron scripts

--- backups/cron/daily.sh
+++ backups/cron/daily.sh
@@ -1,9 +1,19 @@
-sudo /usr/bin/rsnapshot daily
+ #!/bin/bash  
 
-sudo chown -R thomas:thomas /mnt/backups
+USER=thomas
+export XDG_RUNTIME_DIR=/run/user/1000
 
-if [ $? -eq 0 ]; then
-  /usr/bin/notify-send 'Backup' 'Daily backup completed successfully'
+
+if mountpoint -q /media/backups; then
+		sudo /usr/bin/rsnapshot daily
+		sudo chown -R thomas:thomas /media/backups
+		# Check the exit status of the rsnapshot command
+		if [ $? -eq 0 ]; then                                                                                                                                                                                      
+				/usr/bin/notify-send 'rsync' 'Daily backup completed successfully'                                                                                                                                   
+		else                                                                                                                                                                                                       
+				/usr/bin/notify-send -u critical 'rsync' 'Daily backup failed'                                                                                                                                                   
+		fi                      
 else 
-  /usr/bin/notify-send 'Backup' 'Daily backup failed'
+		echo "Backup drive not mounted"
+		/usr/bin/notify-send -u critical 'Backup' 'Error: backup drive not mounted'
 fi
--- backups/cron/weekly.sh
+++ backups/cron/weekly.sh
@@ -1,9 +1,19 @@
-sudo /usr/bin/rsnapshot weekly
+#!/bin/bash  
 
-sudo chown -R thomas:thomas /mnt/backups
+USER=thomas
+export XDG_RUNTIME_DIR=/run/user/1000
 
-if [ $? -eq 0 ]; then
-  /usr/bin/notify-send 'Backup' 'Weekly backup completed successfully'
+
+if mountpoint -q /media/backups; then
+		sudo /usr/bin/rsnapshot weekly
+		sudo chown -R thomas:thomas /media/backups
+		# Check the exit status of the rsnapshot command
+		if [ $? -eq 0 ]; then                                                                                                                                                                                      
+				/usr/bin/notify-send 'rsync' 'Weekly backup completed successfully'                                                                                                                                   
+		else                                                                                                                                                                                                       
+				/usr/bin/notify-send -u critical 'rsync' 'Weekly backup failed'                                                                                                                                                   
+		fi                      
 else 
-  /usr/bin/notify-send 'Backup' 'Weekly backup failed'
+		echo "Backup drive not mounted"
+		/usr/bin/notify-send -u critical 'Backup' 'Error: backup drive not mounted'
 fi
